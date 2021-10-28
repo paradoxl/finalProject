e679e645c911e9350c29f28bf41b5cd8e25e0f02
Michael Evans <Michael.evans1775@outlook.com>
2021-10-28 15:49:23 -0500

    Update main.cpp
    added carbs fats proteins and alcohol

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 using namespace std;
+#include "reset.h"
 
 int main(){
+int carb;
+int protein;
+int fat;
+int calories;
+int alcohol;
+//4 calories per carb
+//9 calories per fat
+//9 calories per protien
+//13 calories per fl oz of alcohol
+
+//calculate caloric  value of macros
+carb = carb *4;
+fat = fat *9;
+protein = protein*9;
+alcohol = alcohol*13;
+
+
+
+
     
 }
